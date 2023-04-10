a28fa08c4ad7d06458f7072380ce59156318712f
BuildTools <unconfigured@null.spigotmc.org>
2023-04-10 21:55:20 +0800

    Initial commit

---
+++ src/main/java/spongy/catmod/MyModClient.java
@@ -0,0 +1,10 @@
+package spongy.catmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/spongy/catmod/catgeneratedata.java
@@ -0,0 +1,11 @@
+package spongy.catmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class catgeneratedata implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
